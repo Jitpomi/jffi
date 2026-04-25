de31a442ba931d6f88d868cbe78015cdb163e619
jermsam <ssali@jitpomi.com>
2026-04-25 16:29:30 -0600

    feat: windows

--- cli/templates/hello/platforms/windows/App.xaml.cs
+++ cli/templates/hello/platforms/windows/App.xaml.cs
@@ -1,27 +1,50 @@
 using Microsoft.UI.Xaml;
-using WinRT;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Controls.Primitives;
+using Microsoft.UI.Xaml.Data;
+using Microsoft.UI.Xaml.Input;
+using Microsoft.UI.Xaml.Media;
+using Microsoft.UI.Xaml.Navigation;
+using Microsoft.UI.Xaml.Shapes;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.ApplicationModel;
+using Windows.ApplicationModel.Activation;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
 
 namespace {{name_pascal}}
 {
-    public sealed class App : Application
+    /// <summary>
+    /// Provides application-specific behavior to supplement the default Application class.
+    /// </summary>
+    public partial class App : Application
     {
-        [System.STAThread]
-        public static void Main(string[] args)
-        {
-            ComWrappersSupport.InitializeComWrappers();
-            Application.Start(_ => new App());
-        }
+        private Window? _window;
 
+        /// <summary>
+        /// Initializes the singleton application object.  This is the first line of authored code
+        /// executed, and as such is the logical equivalent of main() or WinMain().
+        /// </summary>
         public App()
         {
+            InitializeComponent();
         }
 
+        /// <summary>
+        /// Invoked when the application is launched.
+        /// </summary>
+        /// <param name="args">Details about the launch request and process.</param>
         protected override void OnLaunched(Microsoft.UI.Xaml.LaunchActivatedEventArgs args)
         {
             _window = new MainWindow();
             _window.Activate();
         }
-
-        private Window _window = null!;
     }
 }
--- cli/templates/hello/platforms/windows/MainWindow.xaml.cs
+++ cli/templates/hello/platforms/windows/MainWindow.xaml.cs
@@ -1,10 +1,28 @@
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Controls.Primitives;
+using Microsoft.UI.Xaml.Data;
+using Microsoft.UI.Xaml.Input;
+using Microsoft.UI.Xaml.Media;
+using Microsoft.UI.Xaml.Navigation;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
 using uniffi.{{name_snake}}_core;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
 
 namespace {{name_pascal}}
 {
-    public sealed class MainWindow : Window
+    /// <summary>
+    /// An empty window that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainWindow : Window
     {
         private readonly Core _core = new();
         private readonly TextBlock _greetingText = new()
@@ -35,6 +53,7 @@ namespace {{name_pascal}}
 
             Content = stack;
             _greetingText.Text = _core.Greeting();
+            InitializeComponent();
         }
 
         private void RefreshButton_Click(object sender, RoutedEventArgs e)
