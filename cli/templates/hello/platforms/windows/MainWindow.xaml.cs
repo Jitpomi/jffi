65f3c1cff8067c506187580b48cae150de9995e6
jermsam <ssali@jitpomi.com>
2026-04-26 12:24:11 -0600

    feat: windows

--- cli/templates/hello/platforms/windows/MainWindow.xaml.cs
+++ cli/templates/hello/platforms/windows/MainWindow.xaml.cs
@@ -1,5 +1,21 @@
 using Microsoft.UI.Xaml;
-using uniffi.{{name_snake}}_core;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Controls.Primitives;
+using Microsoft.UI.Xaml.Data;
+using Microsoft.UI.Xaml.Input;
+using Microsoft.UI.Xaml.Media;
+using Microsoft.UI.Xaml.Navigation;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+//using uniffi.{{name_snake}}_core;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
 
 namespace {{name_pascal}}
 {
@@ -8,17 +24,15 @@ namespace {{name_pascal}}
     /// </summary>
     public sealed partial class MainWindow : Window
     {
-        private readonly Core _core = new();
-
         public MainWindow()
         {
             InitializeComponent();
-            GreetingText.Text = _core.Greeting();
-        }
+//            GreetingText.Text = _core.Greeting();
+       }
 
         private void RefreshButton_Click(object sender, RoutedEventArgs e)
         {
-            GreetingText.Text = _core.Greeting();
+            GreetingText.Text = "Hello Sara"; //_core.Greeting();
         }
     }
 }
