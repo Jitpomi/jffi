a188b14b30e4b991b47467e3dbdb36376b05f2af
jermsam <ssali@jitpomi.com>
2026-05-02 19:39:04 -0600

    fix(linux): use Gio.ApplicationFlags in GTK4 app template
    Per PyGObject GTK4 docs, Application constructor takes Gio.ApplicationFlags,
    not Gtk.ApplicationFlags. The latter doesn't exist and causes:
      AttributeError: 'gi.repository.Gtk' object has no attribute 'ApplicationFlags'
    
    Also adds Gio to imports for the flags reference.

--- cli/templates/hello/platforms/linux/app.py
+++ cli/templates/hello/platforms/linux/app.py
@@ -2,7 +2,7 @@ import gi
 
 gi.require_version('Gtk', '4.0')
 gi.require_version('Adw', '1')
-from gi.repository import Gtk, Adw
+from gi.repository import Gtk, Adw, Gio
 
 from window import {{name_pascal}}Window
 
@@ -10,7 +10,7 @@ class {{name_pascal}}Application(Adw.Application):
     def __init__(self):
         super().__init__(
             application_id='com.example.{{name_package}}',
-            flags=Gtk.ApplicationFlags.DEFAULT_FLAGS
+            flags=Gio.ApplicationFlags.DEFAULT_FLAGS
         )
         self.window = None
 
